84634f8733d582f7fd536142ea61ab00e3049e67
Codie See <csee@uwalumni.com>
2021-10-07 11:45:25 -1000

    react-map-gl MapLibre example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import {useState} from 'react';
+import {render} from 'react-dom';
+import MapGL, {NavigationControl} from 'react-map-gl';
 
-function App() {
+import ControlPanel from './control-panel';
+
+// / maplibre-gl.css is included by tag in index.html
+// / Alternatively, use CSS loader with the following line:
+// import 'maplibre-gl/dist/maplibre-gl.css';
+
+const OSM_MAP = {
+  version: 8,
+  sources: {
+    osm: {
+      type: 'raster',
+      tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+      tileSize: 256,
+      attribution: '&copy; OpenStreetMap Contributors',
+      maxzoom: 19
+    }
+  },
+  layers: [
+    {
+      id: 'osm',
+      type: 'raster',
+      source: 'osm'
+    }
+  ]
+};
+
+export default function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 40,
+    longitude: -100,
+    zoom: 3.5,
+    bearing: 0,
+    pitch: 0
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MapGL
+        {...viewport}
+        width="100%"
+        height="100%"
+        mapStyle={OSM_MAP}
+        onViewportChange={setViewport}
+      >
+        <NavigationControl style={{padding: 20}} />
+      </MapGL>
+
+      <ControlPanel />
+    </>
   );
 }
 
-export default App;
+export function renderToDom(container) {
+  render(<App />, container);
+}
